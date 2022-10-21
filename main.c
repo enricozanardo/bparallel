1695ecb3c3d4d5fcaf0541d35d3bb0441aa1ddd4
onezerobinary <ezanardo@onezerobinary.com>
2022-10-21 17:10:55 +0200

    use shared variable

--- main.c
+++ main.c
@@ -2,16 +2,23 @@
 #include <omp.h>
 
 int main() {
-    int tid = -1;
-    #pragma omp parallel private(tid) // Start parallel region
+    int tid = -1, sid = 0;
+    
+    #pragma omp parallel private(tid) shared(sid) // Start parallel region
     {
         tid = omp_get_thread_num(); // default is the number of CPU in the machine
         printf("Hello from thread: %d\n", tid);
 
+        sid++;
+
         if (tid == 0) {
             printf("Number of therads = %d\n", omp_get_num_threads());
         }
+
     } // end parallel region: join threads
+
+    printf("sid value: %d\n", sid);
+
     return 0;
 }
 
